27f3df0b335adcf9967d9295806213835eca7f22
Fukuda Naoto <never9612@gmail.com>
2017-10-29 13:47:31 +0900

    Initial Commit

---
+++ simLike/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simLike
+//
+//  Created by Fukuda Naoto on 2017/10/29.
+//  Copyright © 2017年 Fukuda Naoto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
